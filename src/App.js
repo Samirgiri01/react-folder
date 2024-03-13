0be8be15d563ea555ce6e006cb91a8efc94be87a
samirgiri01 <samirgiri617@gmail.com>
2024-03-13 07:29:17 +0545

    add initial file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import  Nav from './components/tools/nav';
+
+function App() {
+  return (
+   <>
+
+   <Nav />
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/tools/nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div>
+      <ul>
+        <li>
+            Home
+        </li>
+        <li>About us</li>
+      </ul>
+    </div>
+  )
+}
